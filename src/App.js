bc9e281600ea082820f1b7eca33bf35b0dec5eef
kevinalbert7 <20ko@live.fr>
2022-10-10 23:18:56 +0200

    calcul of water and dynamic background color

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 
 import Step from "./components/Box"
-// import "./styles/global.css"
 
 const tempMin = -20
 const tempMax = 40
@@ -9,63 +8,75 @@ const heartMin = 80
 const heartMax = 180
 const stepsMin = 0
 const stepsMax = 50000
+const initialWater = 1.5
 
 class App extends Component {
   constructor () {
     super ()
     
     this.state = {
-      water: 0,
+      water: initialWater,
       heart: 120,
       temperature: 10,
       steps: 3000,
-      waterTotal: 1.5
     }
 
-    this.onHeartChange = this.onHeartChange.bind(this);
-    this.calculateWater = this.calculateWater.bind(this);
+    this.onHeartChange = this.onHeartChange.bind(this)
+    this.onStepChange = this.onStepChange.bind(this)
+    this.onTemperatureChangeChange = this.onTemperatureChange.bind(this)
+    this.calculateWater = this.calculateWater.bind(this)
 
   }
   
   onHeartChange(e) {
-    this.setState({heart: e.target.value})
-    this.calculateWater(e.target.value)
+    this.setState({heart: Number(e.target.value)}, () => {
+      this.calculateWater()
+    })
   }
   
   onStepChange(e) {
-    this.setState({steps: e.target.value})
+    this.setState({steps: Number(e.target.value)}, () => {
+      this.calculateWater()
+    })
+    this.calculateWater()
   }
 
   onTemperatureChange(e) {
-    this.setState({temperature: e.target.value})
+    this.setState({temperature: Number(e.target.value)}, () => {
+      this.calculateWater()
+    })
+    this.calculateWater()
   }
 
   calculateWater() {
-    let waterTotal = 1.5
+    const { temperature, heart, steps} = this.state
+    const averageTemp = 20
+    const averageHeart = 120
+    const averageSteps = 10000
+    let sum = 0
 
-    if (this.state.temp > 20) {
-      for (let i = 20; i < tempMax; i ++) {
-        waterTotal += 0.02
-      }
+    if (temperature > averageTemp) {
+      const tempDiff = temperature - 20
+      sum += tempDiff * 0.02
     }
 
-    if (this.state.heart > 120) {
-      for (let i = 120; i < 180; i ++) {
-        waterTotal += 0.0008
-      }
+    if (heart > averageHeart) {
+      const heartDiff = heart - 120
+      sum += heartDiff * 0.0008
     }
 
-    if (this.state.steps > 10000) {
-      for (let i = 10000; i < 50000; i ++) {
-        waterTotal += 0.00002
-      }
+    if (steps > averageSteps) {
+      const stepsDiff = steps - 10000
+      sum += stepsDiff * 0.00002
     }
-    console.log("water =>", this.state.water)
-    console.log("waterTotal =>", waterTotal)
-    return this.setState({water: waterTotal})
+
+    const roundedSum = Number((initialWater + sum).toFixed(2))
+    
+    this.setState({water: roundedSum})
   }
 
   render () {
+    const { water, steps, heart, temperature } = this.state
 
     return (
       <div className="container-fluid">
@@ -73,13 +84,13 @@ class App extends Component {
           <Step 
             icon="local_drink"
             color="#3A85FF"
-            value={this.state.water}
+            value={water}
             unit="L"
           />
           <Step 
             icon="directions_walk"
             color="black"
-            value={this.state.steps}
+            value={steps}
             unit="steps"
             min={stepsMin}
             max={stepsMax}
@@ -88,7 +99,7 @@ class App extends Component {
           <Step 
             icon="favorite"
             color="red"
-            value={this.state.heart}
+            value={heart}
             unit="bpm"
             min={heartMin}
             max={heartMax}
@@ -97,7 +108,7 @@ class App extends Component {
           <Step
             icon="wb_sunny"
             color="yellow"
-            value={this.state.temperature}
+            value={temperature}
             unit="°C"
             min={tempMin}
             max={tempMax}
--- src/components/Box.js
+++ src/components/Box.js
@@ -4,16 +4,24 @@ import "../styles/global.css"
 
 class Step extends Component {
     render() {
+        const { color, icon, min, max, value, unit, onChange } = this.props
+        const waterColor = icon === "local_drink"
+        const percentage = (value - 1.5) / 0.0125
+
         return (
-            <div className='box col-sm-3 col-6'>
-                <span className="material-icons" style={{fontSize: "100px", color: `${this.props.color}`}}>{this.props.icon}</span>
-                <p>{this.props.value}{this.props.unit}</p>
-                {this.props.icon !== "local_drink" && <input 
+            <div className='box col-sm-3 col-6' style={{
+                background: waterColor && `linear-gradient(0deg, rgba(0,69,255,1) ${percentage}%, rgba(170, 170, 170, 1) 100%)`
+                }}
+            >
+                <span className="material-icons" style={{fontSize: "100px", color: `${color}`}}>{icon}</span>
+                <p>{value}{unit}</p>
+                {!waterColor && <input 
                     type="range"
-                    min={this.props.min}
-                    max={this.props.max}
-                    value={this.props.value}
-                    onChange={this.props.onChange}
+                    min={min}
+                    max={max}
+                    value={value}
+                    onChange={onChange}
+                    step={icon === "direction_walk" ? 1000 : 1}
                 />}
             </div>
         )
